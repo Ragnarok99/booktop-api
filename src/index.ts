f6d027212bd1bee6888559b1f23e4077d4131a38
Emmanuel villegas <ragnarok@Emmanuels-Mac-Studio.local>
2023-09-17 11:40:46 -0500

    fix: adjusts port

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,15 @@ import cors from "@elysiajs/cors";
 
 import booksRoutes from "@/routes/books.route";
 
+const { PORT = 8080 } = process.env;
+
 const app = new Elysia();
 
 app.use(cors());
 
 app.group("/api/v1", (app) => app.use(booksRoutes));
 
-app.listen(8080);
+app.listen(PORT);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
